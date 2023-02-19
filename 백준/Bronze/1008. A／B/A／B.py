7376d427871c86fe23ad08e4f17dcdb1c2073632
syLim <124178635+tattoost@users.noreply.github.com>
2023-02-19 18:56:19 +0900

    [Bronze V] Title: A/B, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.py
+++ 백준/Bronze/1008. A／B/A／B.py
@@ -1,2 +1,9 @@
-a, b = map(float, input().split())
-print(round(a/b,9))
+ ### 1 ###
+#a, b = map(float, input().split())
+#print(round(a/b,9))
+
+ ### 2 ###
+#python은 정수 나눗셈 결과를 실수형으로 저장하므로 입력값을 int형으로 받아도 된다
+#python은 나눗셈 할 때 오차 10^-9 이하로 계산해주기 때문에, round()를 쓰지 않아도 된다
+a, b = map(int, input().split())
+print(a/b)
