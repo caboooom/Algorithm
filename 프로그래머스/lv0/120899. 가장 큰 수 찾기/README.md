9aaddbccf80c589c7c15a6004c85eb194afa509d
syLim <124178635+tattoost@users.noreply.github.com>
2023-02-22 22:05:18 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/README.md
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/README.md
@@ -1,6 +1,6 @@
 # [level 0] 가장 큰 수 찾기 - 120899 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120899) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120899#) 
 
 ### 성능 요약
 
--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,8 +1,13 @@
 def solution(array):
     answer = []
     
+#    return [max(array), array.index(max(array))]
+
     answer.append(max(array))
     for i in range(len(array)):
         if array[i] == answer[0]:
             answer.append(i)
+            
+    
     return answer
+
