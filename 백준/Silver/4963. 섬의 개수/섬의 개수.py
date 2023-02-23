b7ac11bc6b098ce9d1d2e637374eedefe24b7f21
syLim <124178635+tattoost@users.noreply.github.com>
2023-02-23 17:04:39 +0900

    [Silver II] Title: 섬의 개수, Time: 96 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,10 +1,11 @@
-import sys
 from collections import deque
+import sys
+sys.setrecursionlimit(10**6)
 
 dx = [-1, -1, -1, 0, 1, 1, 1, 0]
 dy = [-1, 0, 1, 1, 1, 0, -1, -1]
 
-
+## bfs 풀이
 def bfs(x,y):
   if graph[x][y]==0: #이미 방문한 적 있으면 false리턴
     return False
@@ -23,6 +24,19 @@ def bfs(x,y):
         q.append((nx,ny))
   return True #인접한 칸들 모두 방문이 끝나면 true리턴
 
+## dfs 풀이
+def dfs(x, y):
+  if 0 <= x < w and 0 <= y < h and graph[x][y] == 1:
+    graph[x][y] = 0  # 방문 처리
+
+    for i in range(8):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      dfs(nx, ny)
+    return True
+  return False
+
+## 입력케이스별로 출력
 while True:
   h, w = map(int, input().split())
   if w == 0 and h == 0:
