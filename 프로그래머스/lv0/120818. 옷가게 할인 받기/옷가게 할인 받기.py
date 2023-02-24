178070f46c23faa2a9b3b6d1bb6c963f05ac6dc4
syLim <124178635+caboooom@users.noreply.github.com>
2023-02-24 16:51:00 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,10 @@
+def solution(price):
+    
+    if 100000<=price<300000:
+        price *= 0.95
+    if 300000<=price<500000:
+        price *= 0.9
+    if price>=500000:
+        price *= 0.8
+    
+    return int(price)
