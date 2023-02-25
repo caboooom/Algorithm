8e38eca7af28ab8576b458c12f56c6c6992bc67e
syLim <124178635+caboooom@users.noreply.github.com>
2023-02-26 00:14:23 +0900

    [Silver III] Title: 1로 만들기, Time: 528 ms, Memory: 39068 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,15 +1,39 @@
+
 x = int(input())
 
-dp = [x]*(x+1)
-dp[x]=0
+dp = [0]*(x+1)
 
-for i in range(x):
-  curNum = x-i
+for i in range(2,x+1):
+  dp[i]=dp[i-1]+1
+  if i%2==0:
+    dp[i]=min(dp[i],dp[i//2]+1)
+  if i%3==0:
+    dp[i]=min(dp[i],dp[i//3]+1)
 
-  if curNum%3==0:
-    dp[curNum//3] = min(dp[curNum//3], dp[curNum]+1)
-  if curNum%2==0:
-    dp[curNum//2] = min(dp[curNum//2], dp[curNum]+1)
-  dp[curNum-1] = min(dp[curNum-1], dp[curNum]+1)
+print(dp[x])
 
-print(dp[1])
+
+'''
+예전에 작성했던 c++ 코드
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+	
+	vector<int> DP={0,0,1,1};
+	
+	int n; cin >> n;
+
+	for(int i=4;i<n+1;i++) {
+		int result = DP[i-1]+1;
+		if(i%3==0) result= min(result,DP[i/3]+1);
+		if(i%2==0) result = min(result,DP[i/2]+1);
+		DP.push_back(result);
+		}
+		
+	cout<<DP[n];
+	return 0;
+}
+'''
