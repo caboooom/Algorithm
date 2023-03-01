05474ebe4b0e206480e18b1bc5a530288a82d9e3
syLim <124178635+caboooom@users.noreply.github.com>
2023-03-02 00:51:44 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,19 @@
+def solution(s):
+    #풀이 참고
+    answer = len(s)
+    for n in range(1,len(s)//2+1):
+        compressed=""
+        prev = s[:n]
+        cnt = 1
+        for i in range(n,len(s),n):
+            if prev == s[i:i+n]:
+                cnt += 1
+            else:
+                compressed += str(cnt) + prev if cnt>=2 else prev
+                prev = s[i:i+n]
+                cnt = 1
+        compressed += str(cnt) + prev if cnt>=2 else prev
+        
+        answer = min(answer,len(compressed))
+                
+    return answer
