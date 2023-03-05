61f7dfa3e12340c248e8c08002bf0e1068248d40
syLim <124178635+caboooom@users.noreply.github.com>
2023-03-05 18:06:14 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,12 @@
+def solution(A, B):
+    if A==B:
+        return 0
+    
+    answer = 0
+    for i in range(len(A)-1):
+        answer += 1
+        A = A[-1]+A[:-1]
+        if A == B:
+            return answer
+        
+    return -1
