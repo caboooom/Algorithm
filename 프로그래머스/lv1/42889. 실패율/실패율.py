c0bf49016b75025e22853277a857239bd5fb9242
syLim <124178635+caboooom@users.noreply.github.com>
2023-03-09 21:32:09 +0900

    Update 실패율.py

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,22 +1,23 @@
 def solution(N, stages):
     answer = []
     
-    arr1=[0]*502
-    arr2=[0]*502
+    failed=[0]*502
+    cleared=[0]*502
     
     for i in stages:
-        arr1[i] += 1
+        failed[i] += 1
         for j in range(1,i+1):
-            arr2[j] += 1
+            cleared[j] += 1
             
     arr=[]
-    for i in range(1,502):
-        if arr2[i] != 0:
-            arr.append((arr1[i]/arr2[i],i))
-        if arr2[i] == 0 and i<=N:
+    for i in range(1,N+1):
+        if cleared[i] != 0:
+            arr.append((failed[i]/cleared[i],i))
+        else:
             arr.append((0,i))
-            
+    
     arr.sort(key=lambda x:(-x[0],x[1]))
+    
     for rate,num in arr:
         if num<=N:
             answer.append(num)
