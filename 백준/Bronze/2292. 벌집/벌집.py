ac4243eb99276971dc42a284f7b7a3210c2f56cb
syLim <124178635+caboooom@users.noreply.github.com>
2023-03-12 16:13:09 +0900

    [Bronze II] Title: 벌집, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,17 +1,8 @@
-n = int(input())
-if n==1 or n==2:
-  print(n)
-  exit()
-  
-n -= 2
+n=int(input())
 
-increase = 2
-cur=0
-floor=2
-while True:
-  if n//6<=cur:
-    print(floor)
-    exit()
-  cur += increase
+floor = 1
+cur=1
+while n>cur:
+  cur += floor*6
   floor += 1
-  increase += 1
+print(floor)
