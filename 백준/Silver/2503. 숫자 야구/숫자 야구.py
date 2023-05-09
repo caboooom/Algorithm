411db64144caf0fa4b7880be9e5c7e63f5be3d54
syLim <124178635+caboooom@users.noreply.github.com>
2023-05-09 22:39:58 +0900

    [Silver III] Title: 숫자 야구, Time: 64 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,46 +1,38 @@
 import sys
 input = sys.stdin.readline
 
-def check(num, a, b):
-  first = num//100
-  second = (num%100)//10
-  third = num%10
+def check(q, a, b):
+  qnum = [q//100, (q%100)//10, q%10] # 영수가 말한 답
   for i in range(123,988):
-    if len(set([i//100, (i%100)//10, i%10])) != 3 or '0' in str(i):
-      if i in ans_lst:
-        ans_lst.remove(i)
+    if i not in ans_lst:
       continue
     strike, ball = 0, 0
-    if str(first) in str(i):
-      ball += 1
-    if str(second) in str(i):
-      ball += 1
-    if str(third) in str(i):
-      ball += 1
-    if first == i//100:
-      ball -= 1
-      strike += 1
-    if second == (i%100)//10:
-      ball -= 1
-      strike += 1
-    if third == i%10:
-      ball -= 1
-      strike += 1
+    inum = [i//100, (i%100)//10, i%10]
+    for j in range(3):
+      if qnum[j] in inum:
+        ball += 1
+      if qnum[j] == inum[j]:
+        strike += 1
+        ball -= 1
     if strike == a and ball == b:
       pass
     else:
       if i in ans_lst:
         ans_lst.remove(i)
 
+# 정답 후보 리스트
 ans_lst = [i for i in range(123,988)]
-
+for i in range(123,988):
+  if len(set([i//100, (i%100)//10, i%10])) != 3 or '0' in str(i):
+      if i in ans_lst:
+        ans_lst.remove(i)
+        
 n = int(input())
 for _ in range(n):
-  ans, strike, ball = map(int,input().split())
-  if strike == 3:
+  x, y, z = map(int,input().split())
+  if y == 3:
     print(1)
     exit()
-  check(ans,strike,ball)
+  check(x,y,z)
 
 print(len(ans_lst))
-#print(ans_lst)
