bf418f449109495ae5868004d7f95657dada00cb
syLim <124178635+caboooom@users.noreply.github.com>
2023-05-11 01:19:45 +0900

    [Silver III] Title: 달팽이, Time: 1144 ms, Memory: 71016 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,35 +1,32 @@
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
-
 n = int(input())
 table = [[0 for j in range(n)] for i in range(n)]
 
 target = int(input())
 
+# 정가운데 좌표 구하기
 x, y = n//2+1, n//2+1
-
-table[x-1][y-1] = 1
-
-
+# 1을 채워넣기
 val = 1
-for i in range(3,n+1,2): 
-  x, y = x-1, y
-  for j in range(4):
-    if j==0:
-      for k in range(i-2):
-        val += 1
-        table[x-1][y-1]=val
-        x += dx[j]
-        y += dy[j]
-    else:
-      for k in range(i-1):
-        val += 1
-        table[x-1][y-1]=val
-        x += dx[j]
-        y += dy[j]
-  val += 1
-  table[x-1][y-1] = val
+table[x-1][y-1] = val
 
+# 오른쪽, 아래, 왼쪽, 위
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+# 한 껍데기씩 채워넣기
+for i in range(3,n+1,2):
+  # 출발좌표로 이동 (왼쪽 대각선)
+  x, y = x-1, y-1
+  # 오른쪽 - 아래 - 왼쪽 - 위 순서대로
+  for j in range(4):
+    # 각각 n-1칸씩 채우기
+    for k in range(i-1):
+      val += 1        
+      x += dx[j]
+      y += dy[j]
+      table[x-1][y-1]=val
+
+# 결과 출력
 tx, ty = 0,0
 for i in range(n):
   for j in range(n):
@@ -37,6 +34,5 @@ for i in range(n):
     if table[i][j]==target:
       tx,ty = i+1, j+1
   print()
-
-print(tx,ty)
   
+print(tx,ty)
