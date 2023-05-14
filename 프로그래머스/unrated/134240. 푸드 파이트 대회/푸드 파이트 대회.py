7a4d4500c111155060925c8faed2fbf59206e2b0
syLim <124178635+caboooom@users.noreply.github.com>
2023-05-14 14:23:59 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,10 +1,7 @@
 def solution(food):
-    front, end = [], []
+    half = []
     for i in range(1,len(food)):
         for j in range(food[i]//2):
-            front.append(str(i))
-            end.append(str(i))
-    end.reverse()
-    result = front + ['0'] + end # 물은 항상 1개
-    
+            half.append(str(i))
+    result = half + ['0'] + half[::-1] # 물은 항상 1개
     return ''.join(result)
