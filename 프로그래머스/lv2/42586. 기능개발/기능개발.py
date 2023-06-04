1671b70f105e55e7ca85ad35078b09adece160f5
syLim <124178635+caboooom@users.noreply.github.com>
2023-06-04 15:52:50 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,32 +1,23 @@
+from collections import deque
+
 def solution(progresses, speeds):
-    n = len(progresses)
-    answer = []
-    complete = [0]*n # progress별 배포일 기록
-    check = 0 # 배포 완료된 progress 개수
-    days = 1 # 경과 일수
-    while check < n: # 모두 배포 완료될 때까지 반복
-        for i in range(n):
-            if progresses[i] >= 100:
-                continue
-            progresses[i] += speeds[i]
-            if progresses[i] >= 100:
-                complete[i] = days
-                check += 1
-        days += 1
     
-    # 차례대로 배포
-    prev = complete[0]
-    idx = 1
-    cnt = 1
-    while idx < n:
-        if complete[idx] <= prev:
+    progresses = deque(progresses)
+    speeds = deque(speeds)
+    
+    answer = []
+    days = 0
+    cnt = 0
+    while progresses:
+        if progresses[0] + days*speeds[0] >= 100:
+            progresses.popleft()
+            speeds.popleft()
             cnt += 1
         else:
-            answer.append(cnt)
-            cnt = 1
-            prev = complete[idx]
-        idx += 1
-    if cnt > 0:
-        answer.append(cnt)
+            if cnt > 0:
+                answer.append(cnt)
+                cnt = 0
+            days += 1
+    answer.append(cnt)
         
     return answer
