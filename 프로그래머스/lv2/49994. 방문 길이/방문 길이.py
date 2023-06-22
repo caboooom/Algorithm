92262f393e4e4c034669e09fd635ab4418fa66e0
syLim <124178635+caboooom@users.noreply.github.com>
2023-06-22 18:08:45 +0900

    [level 2] Title: 방문 길이, Time: 0.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
@@ -6,21 +6,13 @@ def solution(dirs):
     x = 5
     y = 5
     
-    # 캐릭터가 지나간 좌표 순서대로 구하기
-    visited = [(5,5)] 
+    visited = set() # 방문한 선분
+    prev = (5,5)
     for dir in dirs:
         nx, ny = x + move[dir][0], y + move[dir][1] # 이동좌표
         if 0 <= nx <= 10 and 0 <= ny <= 10: # 범위체크
-            visited.append((nx,ny))
+            visited.add((prev,(nx,ny)))
+            visited.add(((nx,ny),prev))
+            prev = (nx,ny)
             x, y = nx, ny
-
-    # 처음 걷는 길 count
-    line = [(visited[i], visited[i+1]) for i in range(len(visited)-1)]
-    answer = 0
-    check = []
-    for a,b in line:
-        if (a,b) not in check and (b,a) not in check:
-            answer += 1
-            check.append((a,b))
-    
-    return answer
+    return len(visited)//2
