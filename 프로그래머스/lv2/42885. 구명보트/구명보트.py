f601bce43a15226d2cea97cd34779fc41f21d4b4
syLim <124178635+caboooom@users.noreply.github.com>
2023-07-05 16:04:06 +1000

    [level 2] Title: 구명보트, Time: 8.68 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -2,11 +2,12 @@ def solution(people, limit):
     answer = 0
     people.sort(reverse = True)
     # 1명 또는 2명씩 태우기
-    idx = 0
-    while idx < len(people):
-        if people[idx] + people[-1] <= limit:
-            people.pop()
-        idx += 1
+    a = 0
+    b = len(people)-1
+    while a <= b:
+        if people[a] + people[b] <= limit:
+            b -= 1
+        a += 1
         answer += 1
     
     return answer
