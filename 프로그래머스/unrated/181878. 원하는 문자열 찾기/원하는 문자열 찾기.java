cec2ea8592a5630a5934988e0d4171c852ef28e9
syLim <124178635+caboooom@users.noreply.github.com>
2023-08-19 16:47:59 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 4.42 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        myString = myString.toLowerCase();
+        pat = pat.toLowerCase();
+        if (myString.indexOf(pat) != -1) return 1;
+        return 0;
+    }
+}
