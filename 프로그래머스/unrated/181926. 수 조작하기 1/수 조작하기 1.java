7d0a2c06f5425d386efda89291fc06ed68b65ae9
syLim <124178635+caboooom@users.noreply.github.com>
2023-09-11 17:35:59 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.03 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -3,16 +3,35 @@ import java.util.*;
 class Solution {
     public int solution(int n, String control) {
         
-        Map<Character, Integer> m = new HashMap<>();
-        m.put('w',1);
-        m.put('s',-1);
-        m.put('d',10);
-        m.put('a',-10);
+//         Map<Character, Integer> m = new HashMap<>();
+//         m.put('w',1);
+//         m.put('s',-1);
+//         m.put('d',10);
+//         m.put('a',-10);
         
-        for(int i=0; i<control.length(); i++){
-            n += m.get(control.charAt(i));
+//         for(int i=0; i<control.length(); i++){
+//             n += m.get(control.charAt(i));
+//         }
+        
+//         return n;
+        
+        char[] arr = control.toCharArray();
+        for(char c : arr){
+            switch(c){
+                case 'w' :
+                    n += 1;
+                    break;
+                case 's' :
+                    n -= 1;
+                    break;
+                case 'd' :
+                    n += 10;
+                    break;
+                case 'a' :
+                    n -= 10;
+                    break;
+            }
         }
-        
         return n;
     }
 }
