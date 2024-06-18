b47f28365ee06d45f823771c67426a60cbd2dc07
syLim <sammys27@naver.com>
2024-06-18 22:18:48 +0900

    [Bronze I] Title: 도비의 난독증 테스트, Time: 104 ms, Memory: 14052 KB -BaekjoonHub

--- 백준/Bronze/2204. 도비의 난독증 테스트/도비의 난독증 테스트.java
+++ 백준/Bronze/2204. 도비의 난독증 테스트/도비의 난독증 테스트.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -11,19 +9,16 @@ public class Main {
             int n = Integer.parseInt(reader.readLine());
             if (n == 0) break;
 
-            List<String> words = new ArrayList<>();
+            String firstWord = reader.readLine();
 
-            int firstWordIdx = 0;
-            words.add(reader.readLine());
             for (int i = 1; i < n; i++) {
                 String word = reader.readLine();
-                if(word.compareToIgnoreCase(words.get(firstWordIdx)) < 0) {
-                    firstWordIdx = i;
+                if(word.compareToIgnoreCase(firstWord) < 0) {
+                    firstWord = word;
                 }
-                words.add(word);
             }
 
-            System.out.println(words.get(firstWordIdx));
+            System.out.println(firstWord);
         }
     }
 }
