2e89e1c6efa450e4765ff3e45e93b7c7080caeb9
Seyeon Lim <sammys27@naver.com>
2024-08-14 09:09:18 +0900

    [Gold V] Title: 하노이 탑, Time: 5840 ms, Memory: 140596 KB -BaekjoonHub

---
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger n = new BigInteger(reader.readLine());
+        System.out.println((new BigInteger("2").pow(n.intValue()))
+                .subtract(new BigInteger("1")));
+        if(n.intValue() <= 20) {
+            hanoiTower(n.intValue(), 1, 2, 3);
+        }
+    }
+
+    public static void hanoiTower(int n, int origin, int temp, int dest) {
+        if(n == 1) {
+            System.out.println(origin + " " + dest);
+            return;
+        }
+        hanoiTower(n-1, origin, dest, temp);
+        System.out.println(origin + " " + dest);
+        hanoiTower(n-1, temp, origin, dest);
+    }
+}
