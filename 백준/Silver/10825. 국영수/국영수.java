d9e286068163b6e06e5d368fff0fbc8bb003f329
Seyeon Lim <sammys27@naver.com>
2025-01-05 09:21:45 +0900

    [Silver IV] Title: 국영수, Time: 752 ms, Memory: 62720 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.java
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -1,62 +1,63 @@
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        List<Student> lst = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+            lst.add(new Student(tokenizer.nextToken(), Integer.parseInt(tokenizer.nextToken()),
+                    Integer.parseInt(tokenizer.nextToken()), Integer.parseInt(tokenizer.nextToken())));
+        }
 
-class Students implements Comparable<Students>{
-    private String name;
-    private int korean;
-    private int english;
-    private int math;
+        lst.sort(Comparator.comparing(Student::getKorean, Comparator.reverseOrder())
+                .thenComparing(Student::getEnglish)
+                .thenComparing(Student::getMath, Comparator.reverseOrder())
+                .thenComparing(Student::getName));
 
-    public Students(String name, int korean, int english, int math){
+        StringBuilder builder = new StringBuilder();
+        for (Student student : lst) {
+            builder.append(student).append("\n");
+        }
+        System.out.println(builder);
+    }
+}
+
+class Student {
+    String name;
+    int korean;
+    int english;
+    int math;
+
+    public Student(String name, int korean, int english, int math) {
         this.name = name;
         this.korean = korean;
         this.english = english;
         this.math = math;
     }
 
-    @Override
-    public String toString(){
-        return this.name;
+    public String getName() {
+        return name;
+    }
+
+    public int getKorean() {
+        return korean;
+    }
+
+    public int getEnglish() {
+        return english;
+    }
+
+    public int getMath() {
+        return math;
     }
 
     @Override
-    public int compareTo(Students o){
-        if (this.korean != o.korean){
-            return o.korean - this.korean;
-        }
-        if (this.english != o.english){
-            return this.english - o.english;
-        }
-        if (this.math != o.math){
-            return o.math - this.math;
-        }
-        return this.name.compareTo(o.name);
-    }
-}
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-
-        ArrayList<Students> arr = new ArrayList<>();
-
-        for (int i=0; i<n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String name = st.nextToken();
-            int korean = Integer.parseInt(st.nextToken());
-            int english = Integer.parseInt(st.nextToken());
-            int math = Integer.parseInt(st.nextToken());
-            arr.add(new Students(name, korean, english, math));
-        }
-
-        Collections.sort(arr);
-
-        for(int i=0; i<n; i++){
-            System.out.println(arr.get(i));
-        }
+    public String toString() {
+        return name;
     }
 }
