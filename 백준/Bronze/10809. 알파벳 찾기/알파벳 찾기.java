1afc82cd22ea53a2306078fec7dc85d20608fdb6
Seyeon Lim <sammys27@naver.com>
2025-01-22 19:25:50 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,13 +1,24 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String S = reader.readLine();
-        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-        for(char c : alphabet) {
-            System.out.print(S.indexOf(c) + " ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));;
+        char[] input = reader.readLine().toCharArray();
+        int[] alphabets = new int[26];
+        Arrays.fill(alphabets, -1);
+        for (int i = 0; i < input.length; i++) {
+            char c = input[i];
+            if (alphabets[c - 'a'] < 0) {
+                alphabets[c - 'a'] = i;
+            }
         }
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < alphabets.length; i++) {
+            answer.append(alphabets[i]).append(" ");
+        }
+        System.out.println(answer);
     }
 }
