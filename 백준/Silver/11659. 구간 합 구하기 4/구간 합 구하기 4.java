00f67945912d31e19d7077f6bb7716f343b46d9c
Seyeon Lim <sammys27@naver.com>
2025-06-26 09:35:53 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 548 ms, Memory: 53116 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,31 +1,28 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine(), " ");
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
         int n = Integer.parseInt(tokenizer.nextToken());
         int m = Integer.parseInt(tokenizer.nextToken());
-
-        int[] arr = new int[n+1];
-        int[] sumArr = new int[n+1];
-        tokenizer = new StringTokenizer(reader.readLine(), " ");
-        for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(tokenizer.nextToken());
-            sumArr[i] = sumArr[i-1] + arr[i];
+        tokenizer = new StringTokenizer(reader.readLine());
+        int[] sum = new int[n + 1];
+        sum[1] = Integer.parseInt(tokenizer.nextToken());
+        for (int i = 2; i <= n; i++) {
+            sum[i] = sum[i-1] + Integer.parseInt(tokenizer.nextToken());
         }
 
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < m; i++) {
-            tokenizer = new StringTokenizer(reader.readLine(), " ");
-            int a = Integer.parseInt(tokenizer.nextToken());
-            int b = Integer.parseInt(tokenizer.nextToken());
-            builder.append(sumArr[b] - sumArr[a-1]).append("\n");
+            tokenizer = new StringTokenizer(reader.readLine());
+            int start = Integer.parseInt(tokenizer.nextToken());
+            int end = Integer.parseInt(tokenizer.nextToken());
+            builder.append(sum[end] - sum[start - 1]).append("\n");
         }
-
         System.out.println(builder);
     }
 }
