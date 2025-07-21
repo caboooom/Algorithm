7820683f627ee5b0eab00a3218f4f8c50cccf264
Seyeon Lim <sammys27@naver.com>
2025-07-21 19:52:49 +0900

    [Silver V] Title: 나이순 정렬, Time: 652 ms, Memory: 55880 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,49 +1,40 @@
+import java.util.stream.Collectors;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
-        List<Member> lst = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            String[] line = reader.readLine().split(" ");
-            lst.add(new Member(Integer.parseInt(line[0]), line[1], i));
-        }
-        
-        Collections.sort(lst);
-        
         StringBuilder builder = new StringBuilder();
-        for (Member member : lst) {
-            builder.append(member).append("\n");
+
+        List<Member> members = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+            members.add(new Member(Integer.parseInt(tokenizer.nextToken()), tokenizer.nextToken()));
+        }
+
+        List<Member> sortedList = members.stream()
+                .sorted(Comparator.comparing(m -> m.age))
+                .collect(Collectors.toList());
+        for (Member member : sortedList) {
+            builder.append(member.age).append(" ").append(member.name).append("\n");
         }
         System.out.println(builder);
+
     }
 }
 
-class Member implements Comparable<Member> {
+class Member {
     int age;
     String name;
-    int order;
-
-    public Member(int age, String name, int order) {
+    Member(int age, String name) {
         this.age = age;
         this.name = name;
-        this.order = order;
-    }
-
-    @Override
-    public int compareTo(Member o) {
-        if (this.age == o.age) {
-            return this.order - o.order;
-        }
-        return this.age - o.age;
-    }
-
-    @Override
-    public String toString() {
-        return age + " " + name;
     }
 }
