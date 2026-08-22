e6bb0f3266216735b5609c9bd9560fec87801426
Seyeon Lim <sammys27@naver.com>
2026-08-22 10:20:58 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.11 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129) 
 
 ### 성능 요약
 
-메모리: 4.64 MB, 시간: 0.09 ms
+메모리: 4.95 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 20일 21:40:07
+2026년 08월 22일 10:20:58
 
 ### 문제 설명
 
--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -3,30 +3,29 @@
 
 using namespace std;
 
-string binary_change(int num_of_one) {
-  // 1이 될때까지 2로 나눈 나머지를 나열한 뒤, 뒤집으면 됨. 그런데, 뒤집을 필요 없음. 왜냐면 문자열에서 1과 0의 갯수만 중요하기 때문에
+string binary_change(int n) {
     string result;
-    while (num_of_one > 0) {
-        result += to_string(num_of_one % 2);
-        num_of_one /= 2;
+    while (n > 0) {
+        result += to_string(n % 2);
+        n /= 2;
     }
     return result;
-} 
+}
 
 vector<int> solution(string s) {
-    int num_of_zero = 0;
     int count = 0;
+    int zero_count = 0;
     while (s != "1") {
-        int num_of_one = 0;
+        int one_count = 0;
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == '1') {
-                num_of_one += 1;
+                one_count++;
             } else {
-                num_of_zero += 1;
+                zero_count++;
             }
         }
-        s = binary_change(num_of_one);
+        s = binary_change(one_count);
         count++;
     }
-    return {count, num_of_zero};
+    return {count, zero_count};
 }
