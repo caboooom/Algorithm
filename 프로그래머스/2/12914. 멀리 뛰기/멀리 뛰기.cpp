635fc0eeef764143989022c97ea799a7abfc74fc
Seyeon Lim <sammys27@naver.com>
2026-09-01 21:05:06 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.03 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    if (n <= 2) {
+        return n;
+    }
+    vector<long long> dp = vector<long long>(n+1, 0);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i-1] % 1234567 + dp[i-2] % 1234567;
+    }
+    return dp[n] % 1234567;
+}
